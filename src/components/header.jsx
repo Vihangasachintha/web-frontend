cf7d3ee9fdf13c24379384fe4d1f9f9dce97557e
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-12-09 23:54:00 +0530

    Refactor Header component for improved mobile navigation; add hamburger menu icon and adjust layout for better responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,24 @@
 import UserData from "./userData.jsx";
 import { Link, useNavigate } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
     const navigate = useNavigate()
   console.log("Header component loading...");
   return (
-    <header className="w-full h-[100px] shadow-2xl flex">
+    <header className="w-full h-[100px] shadow-2xl flex justify-center relative ">
+        <GiHamburgerMenu className="h-full mx-2 text-3xl md:hidden absolute left-2"/>
         <img onClick={()=>{
             navigate("/")
         }} src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer"/>
-        <div className="w-[calc(100%-80px)] h-full flex justify-center items-center">
+        <div className="w-[calc(100%-80px)] h-full hidden md:flex justify-center items-center">
             <Link to="/" className="text-[20px] font-bold mx-2">Home</Link>
             <Link to="/products" className="text-[20px] font-bold mx-2">Products</Link>
             <Link to="/about" className="text-[20px] font-bold mx-2">About</Link>
             <Link to="/contact" className="text-[20px] font-bold mx-2">Contact</Link>
         </div>
-        <div className="w-[80px] flex justify-center items-center">
+        <div className="w-[80px] hidden md:flex justify-center items-center">
             <Link to="/cart" className="text-[20px] font-bold mx-2">
             <BsCart3/>
             </Link>
--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,39 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
-import { useState } from "react";
-import mediaUpload from "../utils/mediaUpload";
+
 
 export default function TestPage() {
-  const [image, setImage] = useState(null);
-
-  function fileUpload() {
-    mediaUpload(image).then(
-      (res)=>{
-        console.log(res)
-      }
-    ).catch(
-      (res)=>{
-        console.log(res)
-      }
-    )
-  }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col">
-      <input
-        type="file"
-        name=""
-        id=""
-        className="file-input file-input-bordered w-full max-w-xs"
-        onChange={(e) => {
-          setImage(e.target.files[0]);
-        }}
-      />
-      <button
-        onClick={fileUpload}
-        className="bg-green-500 text-white font-bold py-2 px-4 rounded"
-      >
-        Upload
-      </button>
+    <div className="w-full h-screen flex justify-center items-center flex-col bg-red-500 md:bg-blue-500 lg:bg-green-500 xl:bg-yellow-500">
+      
     </div>
   );
 }
+ 
